fd7ca0d3633d2526737d3be2f02af733323048e1
Jesus Galvis <74638215+Jesus9464@users.noreply.github.com>
2023-06-20 11:19:37 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { show } from "@techtivo/pfm-sdk";
+
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
+  const partnerURL = "https://mfe-pfm.lendingpoint.com/pfm-dev";
+  const partnerToken = "partner-token";
+  const userToken = "user-token";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <img src={logo} alt="logo" />
+      <div className="container_button">
+        <button
+          onClick={() => {
+            show(partnerURL, partnerToken);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          new user
+        </button>
+        <button
+          className="button_2"
+          onClick={() => {
+            show(partnerURL, partnerToken, userToken);
+          }}
+        >
+          registered user
+        </button>
+      </div>
     </div>
   );
 }
