bcf45a02007cfbce9ae4329e88c6e20b8392d35d
Jesus Galvis <74638215+Jesus9464@users.noreply.github.com>
2023-07-12 09:15:26 -0500

    feat: add pfm sdk web

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import React from "react";
-import { show } from "@techtivo/pfm-sdk";
+import { show, onMessagesListener } from "@techtivo/pfm-sdk";
 
 import logo from "./logo.png";
 import "./App.css";
 
 function App() {
-  const partnerURL = "https://mfe-pfm.lendingpoint.com/pfm-dev";
-  const partnerToken = "partner-token";
-  const userToken = "user-token";
+  const data = {
+    partnerURL: "https://mfe-pfm.lendingpoint.com/pfm-dev",
+    partnerToken: "69778153-40e7-4fa4-a2dc-d623328bd84f",
+    userToken: "bd8c821d-2f3c-4454-877e-4301ba7aabac",
+  };
+
+  const message = (message: string) => {
+    console.log(message);
+  };
+
+  onMessagesListener(message);
 
   return (
     <div className="App">
+      <h1 style={{ color: "white" }}>React</h1>
       <img src={logo} alt="logo" />
       <div className="container_button">
         <button
           onClick={() => {
-            show(partnerURL, partnerToken);
+            show(data);
           }}
         >
           new user
-        </button>
-        <button
-          className="button_2"
-          onClick={() => {
-            show(partnerURL, partnerToken, userToken);
-          }}
-        >
-          registered user
         </button>
       </div>
     </div>
